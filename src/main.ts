738bf993763ab3d50a3668910e09ffaa46007342
kay <kayzhang@trussan.com>
2021-12-17 12:02:21 +0800

    feat：添加乾坤相关配置

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,78 @@
-import Vue from 'vue'
 import { createApp } from 'vue'
 import App from './App.vue'
-import { Tabbar, TabbarItem, Calendar, ConfigProvider, Icon } from 'vant'
+import {
+  Tabbar,
+  TabbarItem,
+  Calendar,
+  ConfigProvider,
+  Icon,
+  ActionSheet,
+  Search,
+  Toast,
+  Cell,
+  CellGroup,
+  Button,
+  Popup,
+  Image,
+  Lazyload
+} from 'vant'
 import routes from './router'
 import 'vant/lib/index.css'
-import { renderWithQiankun, qiankunWindow } from 'vite-plugin-qiankun/dist/helper'
-import { createRouter, createWebHistory } from 'vue-router';
+import {
+  renderWithQiankun,
+  qiankunWindow
+} from 'vite-plugin-qiankun/dist/helper'
+import { createRouter, createWebHistory } from 'vue-router'
 
+let router = null
+let instance = null
+let history = null
 
-let router = null;
-let instance = null;
-let history = null;
+instance = createApp(App)
+instance.use(Tabbar)
+instance.use(TabbarItem)
+instance.use(Calendar)
+instance.use(ConfigProvider)
+instance.use(Icon)
 
-instance = createApp(App);
-instance.use(Tabbar);
-instance.use(TabbarItem);
-instance.use(Calendar);
-instance.use(ConfigProvider);
-instance.use(Icon);
+instance.use(ActionSheet)
+instance.use(Search)
+instance.use(Toast)
+instance.use(Cell)
+instance.use(CellGroup)
+instance.use(Button)
+instance.use(Popup)
+instance.use(Image)
 
+instance.use(Lazyload, {
+  lazyComponent: true
+})
+
+declare global {
+  interface Window {
+    Explain: any
+    __POWERED_BY_QIANKUN__: any
+    __INJECTED_PUBLIC_PATH_BY_QIANKUN__: any
+    wx: any
+  }
+}
 
 function render(props = {}) {
-  const { container } = props;
-  history = createWebHistory(qiankunWindow.__POWERED_BY_QIANKUN__ ? '/calendar-mobile' : '/');
+  const { container } = props
+  history = createWebHistory(
+    qiankunWindow.__POWERED_BY_QIANKUN__ ? '/calendar-mobile' : '/'
+  )
   //   console.log(history,qiankunWindow.__POWERED_BY_QIANKUN__ ,"history");
   router = createRouter({
     history,
-    routes,
-  });
+    routes
+  })
 
-
-  instance.use(router);
+  instance.use(router)
   //   instance.use(store);
-  instance.mount(container ? container.querySelector('#app') : document.getElementById("app"));
+  instance.mount(
+    container ? container.querySelector('#app') : document.getElementById('app')
+  )
   if (qiankunWindow.__POWERED_BY_QIANKUN__) {
     console.log('我正在作为子应用运行')
   }
@@ -41,22 +81,22 @@ function render(props = {}) {
 // some code
 renderWithQiankun({
   mount(props) {
-    console.log("viteapp mount");
-    render(props);
+    console.log('viteapp mount')
+    render(props)
   },
   bootstrap() {
-    console.log('bootstrap');
+    console.log('bootstrap')
   },
   unmount(props) {
-    console.log("vite被卸载了");
-    instance.unmount();
-    instance._container.innerHTML = '';
-    history.destroy();// 不卸载  router 会导致其他应用路由失败
-    router = null;
-    instance = null;
-  },
-});
+    console.log('vite被卸载了')
+    instance.unmount()
+    instance._container.innerHTML = ''
+    history.destroy() // 不卸载  router 会导致其他应用路由失败
+    router = null
+    instance = null
+  }
+})
 
 if (!qiankunWindow.__POWERED_BY_QIANKUN__) {
-  render();
+  render({})
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import qiankun from 'vite-plugin-qiankun';
-import { resolve } from "path";
-const { name } = require('./package.json');
+import qiankun from 'vite-plugin-qiankun'
+import { resolve } from 'path'
+const { name } = require('./package.json')
 // useDevMode 开启时与热更新插件冲突
-const useDevMode = true     // 如果是在主应用中加载子应用vite,必须打开这个,否则vite加载不成功, 单独运行没影响
+const useDevMode = true // 如果是在主应用中加载子应用vite,必须打开这个,否则vite加载不成功, 单独运行没影响
 
 function pathResolve(dir: string) {
   return resolve(__dirname, '.', dir)
@@ -22,24 +22,20 @@ export default defineConfig(({ mode }) => {
       }
     },
     server: {
-      host: '0.0.0.0',// 暴露内网ip
+      host: '0.0.0.0', // 暴露内网ip
       port: 3000,
       cors: true,
-      origin: 'http://localhost:3000/'
+      origin: 'https://blue.planplus.cn/chat-tool-bar/calender-mobile'
     },
     output: {
       // 把子应用打包成 umd 库格式
       library: `${name}-[name]`,
       libraryTarget: 'umd',
-      jsonpFunction: `webpackJsonp_${name}`,
+      jsonpFunction: `webpackJsonp_${name}`
     },
     define: {
       'process.env': env
     }
-  },
-    server: {
-      port: 3000,
-      host: '0.0.0.0'
-    }
+  }
   return config
 })
